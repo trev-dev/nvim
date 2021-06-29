60cf1d8d4cdff2740e21d01bbb9172a2ccc7818e
Trevor Richards <trev@trevdev.ca>
2021-06-28 17:11:15 -0700

    drop parens because lua and why not

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -1,12 +1,13 @@
 -- Packages
-require('plugins.packages')
+require'plugins.packages'
 
 -- Configs
-require('plugins.colorizer')
-require('plugins.gitsigns')
-require('plugins.lsp')
-require('plugins.kommentary')
-require('plugins.fugitive')
-require('plugins.telescope')
-require('plugins.vim-compe')
-require('plugins.galaxyline')
+require'plugins.colorizer'
+require'plugins.gitsigns'
+require'plugins.lsp'
+require'plugins.kommentary'
+require'plugins.fugitive'
+require'plugins.telescope'
+require'plugins.vim-compe'
+require'plugins.galaxyline'
+require'plugins.emmet'
