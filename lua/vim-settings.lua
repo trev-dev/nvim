a4034efd4268ea0465bac570bb515395d11395e4
Trevor Richards <trev@trevdev.ca>
2021-06-28 17:14:09 -0700

    use global to resolve unsetting of tab settings

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -3,8 +3,13 @@ local wo = vim.wo
 local bo = vim.bo
 
 -- Global
-o.listchars='eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣'
 o.termguicolors = true
+o.listchars='eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣'
+o.expandtab = true
+o.shiftwidth = 2
+o.softtabstop = 2
+o.tabstop = 2
+o.lbr = true
 
 -- Window
 wo.breakindent = true
@@ -13,8 +18,3 @@ wo.linebreak = true
 wo.number = true
 wo.relativenumber = true
 
--- Buffers
-bo.expandtab = true
-bo.shiftwidth = 2
-bo.softtabstop = 2
-bo.tabstop = 2
