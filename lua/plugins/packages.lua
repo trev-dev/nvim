e428b396d08baa757c995e7e38de29c31aac52ae
Trevor Richards <trev@trevdev.ca>
2021-06-28 17:12:53 -0700

    add treesitter <3

--- lua/plugins/packages.lua
+++ lua/plugins/packages.lua
@@ -49,6 +49,10 @@ packer.startup(function(use)
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
   }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
   -- LSP & Completion
   use 'hrsh7th/nvim-compe'
   use 'hrsh7th/vim-vsnip'
