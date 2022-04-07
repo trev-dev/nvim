0a6ff1f23b588a193daf0f45d844cc9eafe6239f
Trevor Richards <trev@trevdev.ca>
2022-04-07 11:55:02 -0700

    character for blanklines

--- lua/packages/indent-blankline.lua
+++ lua/packages/indent-blankline.lua
@@ -5,9 +5,10 @@ return {
   setup = function()
     indentbl.setup{
       show_current_context = true,
-      show_current_context_start = true,
       use_treesitter = true,
-      show_trailing_blankline_indent = true
+      show_trailing_blankline_indent = true,
+      char_blankline = '┆',
+      context_char_blankline = '┆'
     }
 
     g.indent_blankline_filetype_exclude = {
@@ -19,7 +20,7 @@ return {
       "md",
       "org",
       "norg",
-      "",
+      ""
     }
   end
 }
