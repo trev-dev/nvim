d932f6473cc72b990c4577ab9a56b1744c29b07e
Trevor Richards <trev@trevdev.ca>
2022-10-14 11:11:09 -0700

    prune lua files

--- lua/theme.lua
+++
@@ -1,9 +0,0 @@
-local material = require'material'
-local g = vim.g
-local cmd = vim.cmd
-
-material.setup{}
-
-g.material_style = 'darker'
-cmd[[colorscheme material]]
--- cmd[[au VimEnter * highlight ColorColumn guibg=#171717]]
