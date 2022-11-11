0e2ceea68b5ea69e6c34f3efd1bfc774153d8446
Trevor Richards <trev@trevdev.ca>
2022-11-11 08:35:42 -0800

    theme scrollbar search results

--- lua/package/nvim-scrollbar.lua
+++ lua/package/nvim-scrollbar.lua
@@ -1,7 +1,19 @@
 local ok, scrollbar = pcall(require, "scrollbar")
 if not ok then return end
 
-scrollbar.setup({ handlers = { diagnostic = true, search = true } })
+local conf = {
+  handle = { color = "#323232" },
+  handlers = { diagnostic = true, search = true }
+}
+
+local useTheme, colors = pcall(require, "material.colors")
+if useTheme then
+  conf.marks = {
+    Search = { color = colors.editor.fg }
+  }
+end
+
+scrollbar.setup(conf)
 
 if pcall(require, "hlslens") == true then
   require("scrollbar.handlers.search").setup()
