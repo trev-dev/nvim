d8c75c9bac45636af751342750dca44c10069b3e
Trevor Richards <trev@trevdev.ca>
2022-11-11 08:35:13 -0800

    disable highlighting on large source files

--- lua/package/treesitter.lua
+++ lua/package/treesitter.lua
@@ -1,8 +1,14 @@
 local ok = pcall(require, "nvim-treesitter")
 if not ok then return end
 
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-local M = {}
+local disable = function(lang, buf)
+  if lang == "help" then return true end
+  local max_filesize = 100 * 1024 -- 200 KB
+  local fs, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  if fs and stats and stats.size > max_filesize then
+    return true
+  end
+end
 
 require("nvim-treesitter.configs").setup({
   ensure_installed = {
@@ -28,6 +34,6 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = {"org"},
-    disable = {"help"}
+    disable = disable
   }
 })
