f7ac74a4e3bf815ce311614be5ac7dbbafcdf929
Trevor Richards <trev@trevdev.ca>
2023-02-03 17:03:56 -0800

    make nvim-tree wider

--- lua/package/nvim-tree.lua
+++ lua/package/nvim-tree.lua
@@ -1,7 +1,11 @@
 local ok, nvt = pcall(require, "nvim-tree")
 if not ok then return end
 
-nvt.setup()
+nvt.setup {
+  view = {
+    width = 50
+  }
+}
 
 vim.keymap.set("n", "<C-k>b", ":NvimTreeToggle<cr>")
 
